2c604627bc949e807a0d9bb85741ac3b8b1d1f11
Bruno José Borges <bruno.borgez3007@gmail.com>
2024-01-17 17:27:23 -0300

    Create e2e tests with cypress

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  e2e: {
+    baseUrl: 'https://notes-serverless-app.com'
+  },
+  env: {
+    viewportWidthBreakpoint: 768,
+  },
+})
